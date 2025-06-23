38813aadfab40ba2a766a9d78918addf07f833a6
alexandernovadev <alexsk88.dev@gmail.com>
2025-06-22 19:33:04 -0500

    Update Input and Select components to improve padding and focus styles for enhanced user experience. Adjust input height and select trigger padding for better visual consistency across the UI.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-8 w-full rounded-md border-2 border-input bg-background px-2 py-1 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:shadow-[0_0_15px_rgba(34,197,94,0.5)] transition-all duration-250 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex h-8 w-full rounded-md border-2 border-input bg-background px-2 py-4 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:shadow-[0_0_15px_rgba(34,197,94,0.5)] transition-all duration-250 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
