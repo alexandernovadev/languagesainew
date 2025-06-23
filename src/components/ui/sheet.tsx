7103b49f74cf8445ac837fe050d2aa63070485ca
alexandernovadev <alexsk88.dev@gmail.com>
2025-06-23 14:23:05 -0500

    Refactor utility imports in multiple UI components to a new common structure, enhancing code organization and maintainability.

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -2,8 +2,7 @@ import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
-
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/common/classnames";
 
 const Sheet = SheetPrimitive.Root;
 
