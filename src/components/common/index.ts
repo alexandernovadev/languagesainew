b53b7f31436a2f69e856179d6943878f2f4b90e0
alexandernovadev <alexsk88.dev@gmail.com>
2025-08-30 15:10:54 -0500

    remove files

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,3 @@
-export { GrammarExplanationTooltip } from "./GrammarExplanationTooltip";
 export { TruncatedText } from "./TruncatedText";
 export { TruncatedBadge } from "./TruncatedBadge";
 export { AILoadingContainer, useAILoading } from "./AILoadingContainer";
--- src/components/statistics/index.ts
+++
@@ -1,8 +0,0 @@
-export { StatisticsDashboard } from "./StatisticsDashboard";
-export { StatsKPIs } from "./StatsKPIs";
-export { StatsLevelChart } from "./StatsLevelChart";
-export { StatsQualityChart } from "./StatsQualityChart";
-export { StatsLanguageChart } from "./StatsLanguageChart";
-export { StatsLevelDistribution } from "./StatsLevelDistribution";
-export { StatsLectureMetrics } from "./StatsLectureMetrics";
-export { StatsWordMetrics } from "./StatsWordMetrics";
